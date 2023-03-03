6ef815b5ae1c460032191ea0bb169d18bccb4db5
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-03-03 16:02:09 +0100

    feat: add coockie clicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
+
 function App() {
+
+  const [count, setCount] = useState(0)
 
   return (
     <div className="container px-4 mx-auto">
       <p className='font-bold text-3xl'><span className='text-blue-500'>Hybrid</span><span className='text-blue-900'>Planner</span></p>
-      <button className="btn">Button</button>
+      <div>
+        <button className="btn btn-primary btn-lg text-6xl" onClick={() => { setCount(count + 1) }}>
+          {count}
+        </button>
+      </div>
     </div>
   )
 }

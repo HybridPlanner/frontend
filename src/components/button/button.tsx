616ec5de60aa2fa8ffbd7008a759a1a072f3a474
Louis Bailleau <louis.bailleau@free.fr>
2023-04-28 14:08:54 +0200

    feat: ✨ add input field component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -13,7 +13,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
         ref={ref}
         className={classNames(
-          "bg-blue-600 text-white px-4 py-3.5 rounded-lg font-semibold",
+          "bg-blue-600 text-white px-4 py-2.5 rounded-lg font-semibold",
           "hover:bg-blue-500 transition-colors ease-in-out duration-150",
           "disabled:bg-gray-300 disabled:cursor-not-allowed disabled:text-gray-500",
           "inline-flex gap-1",

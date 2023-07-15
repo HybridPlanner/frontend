719523aadb52084e9570e2a97ac2afa27a8fa323
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-07-15 14:23:59 +0200

    feat(home): :sparkles: navigate to login page when we click on Try HybridPlanner button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar"
 import { Button } from "../components/base/button/button"
 import { useState } from "react";
@@ -5,6 +6,11 @@ import { useState } from "react";
 export default function HomePage() {
   const [name, setName] = useState("Louis DePierre");
   const [email, setEmail] = useState("louis@depierre.com");
+  const navigate = useNavigate();
+  
+  const handleTryButtonClick = () => {
+    navigate("/login");
+  }
   
   return (
     <div className="container px-4 mx-auto flex flex-col gap-3">
@@ -15,7 +21,7 @@ export default function HomePage() {
           Rainbow Meetings with external participants made easy.
         </div>
         <p className="mt-5">Lorem ipsum dolor sit amet consectetur. Erat felis faucibus lorem elementum egestas dictumst pellentesque faucibus. Etiam dui leo vulputate pellentesque lectus cursus massa. Lectus dui tristique sit enim ut. Nisl sodales felis tortor volutpat facilisi velit orci. Imperdiet vitae imperdiet ultrices sed euismod.</p>
-        <Button className="mt-4">Try HybridPlanner</Button>
+        <Button className="mt-4" onClick={handleTryButtonClick}>Try HybridPlanner</Button>
       </div>
       <div className="-z-50 absolute bottom-0 left-0 w-full h-full bg-gradient-to-tr from-blue-200 via-blue-50 to-transparent blur-md"></div>
     </div>

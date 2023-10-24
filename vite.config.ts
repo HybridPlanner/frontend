d385f2788efdf68c04f9806e5f29127ad30480c6
Samy Mosa <samymosa02@gmail.com>
2023-10-24 18:03:10 +0200

    feat: :technologist: Add Docker support

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-console.log(process.env.API_URL)
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  server: {
-    proxy: {
-      '^/api/.*': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+    server: {
+      proxy: {
+        '^/api/.*': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, '')
+        }
       }
     }
   }

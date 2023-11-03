c191dbf07932d95f3b3bea0e6794b347dbe8a46c
Franck G <gutmann.franck@outlook.fr>
2023-11-03 11:20:33 +0100

    feat(meeting): join meeting when countdown finished

--- src/components/meeting/MeetingCountdown.tsx
+++ src/components/meeting/MeetingCountdown.tsx
@@ -1,6 +1,7 @@
 import { Meeting } from "@/types/Meeting";
 import { Card } from "../card/card";
 import Countdown from "react-countdown";
+import { Button } from "../base/button/button";
 
 const labels = ["days", "hours", "minutes", "seconds"];
 
@@ -10,39 +11,48 @@ export function MeetingCountdown({
   meeting: Meeting;
 }): JSX.Element {
   return (
-    <Card className="my-5 xl:my-8 !py-3 shadow-xl">
-      <Countdown
-        date={meeting.start_date}
-        renderer={({ days, hours, minutes, seconds, completed }) => {
-          if (completed) {
-            return <span>Meeting started</span>;
-          } else {
-            return (
-              <div className="flex flex-col justify-center gap-4">
-                <p className="text-lg text-slate-500 text-center">
-                  Starting in
-                </p>
-                <div className="flex item-center">
-                  {[days, hours, minutes, seconds].map((value, index) => {
-                    if (index > 0 || value > 0) {
-                      return (
-                        <CountdownValue value={value} label={labels[index]} />
-                      );
-                    }
-                  })}
-                </div>
-              </div>
-            );
-          }
-        }}
-      />
+    <Card className="my-5 xl:my-8 !py-3 shadow-xl w-full max-w-md">
+      <div className="flex flex-col justify-center gap-4">
+        <Countdown
+          date={meeting.start_date}
+          renderer={({ days, hours, minutes, seconds, completed }) => {
+            if (completed) {
+              return (
+                <>
+                  <p className="text-lg text-slate-500 text-center">
+                    Meeting started !
+                  </p>
+                  <Button>Join meeting</Button>
+                </>
+              );
+            } else {
+              return (
+                <>
+                  <p className="text-lg text-slate-500 text-center">
+                    Starting in
+                  </p>
+                  <div className="flex items-center justify-around">
+                    {[days, hours, minutes, seconds].map((value, index) => {
+                      if (index > 0 || value > 0) {
+                        return (
+                          <CountdownValue value={value} label={labels[index]} />
+                        );
+                      }
+                    })}
+                  </div>
+                </>
+              );
+            }
+          }}
+        />
+      </div>
     </Card>
   );
 }
 
 function CountdownValue({ value, label }: { value: number; label: string }) {
   return (
-    <div className="flex flex-col gap-2 items-center w-16 xl:w-24">
+    <div className="flex flex-col gap-2 items-center">
       <span className="text-4xl xl:text-6xl text-blue-500">{value}</span>
       <span className="text-sm text-gray-500">{label}</span>
     </div>
--- src/components/meeting/MeetingWaiting.tsx
+++ src/components/meeting/MeetingWaiting.tsx
@@ -27,15 +27,15 @@ export function MeetingWaiting({ meeting }: { meeting: Meeting }): JSX.Element {
         </p>
       </div>
 
-      <div className="flex flex-col gap-3 lg:basis-1/3 max-lg:pt-10">
-        <p className="text-indigo-900 font-medium">
+      <div className="flex flex-col gap-3 lg:basis-1/3 max-lg:pt-10 max-lg:items-center">
+        <p className="text-indigo-900 font-medium text-center md:text-left">
           Don’t know HybridPlanner ?
         </p>
-        <h3 className="text-blue-500 text-2xl md:text-3xl font-normal leading-tight break-words">
+        <h3 className="text-blue-500 text-2xl md:text-3xl font-normal leading-tight break-words text-center md:text-left">
           Plan your next Raindow meeting easily with{" "}
           <span className="font-bold">Hybrid</span>Planner
         </h3>
-        <p className="text-slate-500">
+        <p className="text-slate-500 text-center md:text-left">
           HybridPlanner let you plan meetings with external participants easily.
           It’s free.
         </p>

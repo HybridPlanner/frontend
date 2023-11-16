616ec2c3af3bc3ed9440d0cb04e27e7399c865f2
Louis Bailleau <louis.bailleau@free.fr>
2023-11-16 16:46:48 +0100

    fix: wrong component property

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,5 @@
 import { ErrorComponent } from "@/components/ErrorComponent";
 
 export default function ErrorPage() {
-  return <ErrorComponent error={new Error("404 Not found")} />;
+  return <ErrorComponent error={"404 Not found"} />;
 }

3290e544f489980e4ea7639a5c4ac0408a3fd006
Samy MOSA <samymosa02@gmail.com>
2024-01-14 15:29:19 +0100

    fix: :bug: Rewrite api paths in development mode

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => {
         '^/api/.*': {
           target: env.VITE_API_BASE_URL ?? "http://localhost:8080",
           changeOrigin: true,
+          ...(mode === 'development' && { rewrite: (path) => path.replace(/^\/api/, '') })
         }
       }
     }
